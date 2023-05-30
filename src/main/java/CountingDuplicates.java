eb55f156c0cef17867c7dce572b72863798f590c
Max Burrows <maxajburrows@gmail.com>
2023-05-30 22:03:11 +0200

    Solution using streams

--- src/main/java/CountingDuplicates.java
+++ src/main/java/CountingDuplicates.java
@@ -1,21 +1,12 @@
+import java.util.*;
+
 public class CountingDuplicates {
 
     public static int duplicateCount(String text) {
-        return recursionHelped(text, 0);
-    }
-
-    private static int recursionHelped(String text, int counter) {
-        Outer: for (int i = 0; i < text.length(); i++) {
-            for (int j = i + 1; j < text.length(); j++) {
-                if (String.valueOf(text.charAt(i)).equalsIgnoreCase(String.valueOf(text.charAt(j)))) {
-                    String shortenedText = text.replaceAll(
-                            String.format("(?i)[%s]", String.valueOf(text.charAt(i))), "");
-                    counter++;
-                    counter = recursionHelped(shortenedText, counter);
-                    break Outer;
-                }
-            }
-        }
-        return counter;
+        List<String> charList = Arrays.asList(text.toUpperCase().split(""));
+        return (int) charList.stream()
+                .filter(letter -> Collections.frequency(charList, letter) > 1)
+                .distinct()
+                .count();
     }
 }
