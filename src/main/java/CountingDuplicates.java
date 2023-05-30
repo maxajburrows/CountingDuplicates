1df94db60245b5a41d27fa2a8b9e5a817aeea6c6
Max Burrows <maxajburrows@gmail.com>
2023-05-30 22:17:01 +0200

    Complete kata with hashmap and streams

--- src/main/java/CountingDuplicates.java
+++ src/main/java/CountingDuplicates.java
@@ -3,10 +3,12 @@ import java.util.*;
 public class CountingDuplicates {
 
     public static int duplicateCount(String text) {
-        List<String> charList = Arrays.asList(text.toUpperCase().split(""));
-        return (int) charList.stream()
-                .filter(letter -> Collections.frequency(charList, letter) > 1)
-                .distinct()
+        HashMap<Character, Boolean> charCountMap = new HashMap<Character, Boolean>();
+        for (char c : text.toUpperCase().toCharArray()) {
+            charCountMap.put(c, charCountMap.containsKey(c) ? true : false);
+        }
+        return (int) charCountMap.values().stream()
+                .filter(bool -> bool)
                 .count();
     }
 }
